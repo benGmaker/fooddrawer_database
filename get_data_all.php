3b2e295119357216ca6caa5df1662e4d050f3b97
jant86 <j.timmers@student.tue.nl>
2021-03-14 12:33:42 +0100

    hoi

--- get_data_all.php
+++ get_data_all.php
@@ -7,14 +7,16 @@ if ($conn->connect_error) {
 
 $user_id = $_GET['user_id'];
 //Show the last item of each food_id
-$sql = "SELECT masses.food_id,masses.mass,food.unit,food.name
-        FROM food 
-        INNER JOIN masses ON masses.food_id=food.food_id 
-        WHERE (food.user_id = 1 OR food.IP = '" . $_SERVER['REMOTE_ADDR'] . "')
+$sql = "SELECT food.food_id,masses.mass,food.unit,food.name
+        FROM masses 
+        RIGHT JOIN food ON food.food_id=masses.food_id 
+        WHERE ((food.user_id = $user_id OR food.IP = '" . $_SERVER['REMOTE_ADDR'] . "')
         		AND
               masses.id IN (SELECT MAX(masses.id)
                             FROM masses
-                            GROUP BY masses.food_id)
+                            RIGHT JOIN food ON food.food_id = masses.food_id
+                            GROUP BY masses.food_id))
+              OR food.food_id NOT IN (SELECT masses.food_id FROM masses)
         ORDER BY food.name";
 
 $result = mysqli_query($conn, $sql);
